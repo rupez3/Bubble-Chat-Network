a92c19f7c0519ccbf7051d143be3c962cf89e814
Rupesh Chhetri <Rupesh_Chhetri@cable.comcast.com>
2016-10-19 23:30:55 -0400

    completed facebook authentication

--- Bubble Social Networking/SigninVC.swift
+++ Bubble Social Networking/SigninVC.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import FBSDKCoreKit
+import FBSDKLoginKit
 
 class SigninVC: UIViewController {
 
@@ -20,6 +23,33 @@ class SigninVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func facebookAction(_ sender: AnyObject) {
+        
+        let fbLogin = FBSDKLoginManager()
+        fbLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("rkc: failed to authenticate, \(error)")
+            } else if result?.isCancelled == true {
+                print("rkc: user cancelled Facebook authernication")
+            } else {
+                print("rkc: Facebook authentication successful")
+                
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                
+                self.firebaseAuth(credential)
+            }
+        }
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("rkc: Firebase Auhentication Failed")
+            } else {
+                print("rkc: Firebase Authenication Successful")
+            }
+        })
+    }
 
 }
 
