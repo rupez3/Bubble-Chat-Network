3b475f41a93c1e187b7c11c4df8570db7a5e411a
Rupesh Chhetri <Rupesh_Chhetri@cable.comcast.com>
2016-10-20 23:28:26 -0400

    added tabviewcontroller, navigationviewcontroller, feedsvc and messagevc

---
+++ Bubble Social Networking/FeedsVC.swift
@@ -0,0 +1,43 @@
+//
+//  FeedsVC.swift
+//  Bubble Social Networking
+//
+//  Created by Chhetri, Rupesh (Contractor) on 10/20/16.
+//  Copyright © 2016 RupeshChhetri. All rights reserved.
+//
+
+import UIKit
+
+class FeedsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        
+//        self.tabBarController?.navigationItem.hidesBackButton = true
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
